cc3cf2c100630735f9d3aa539b6c5d4bb7649d86
A-Red-Hen <99688148+A-Red-Hen@users.noreply.github.com>
2024-04-04 02:21:24 +0000

    combined vulenrabiloity map function, added 2016-08-31 modified gpkg

--- pages/0_frp.py
+++ pages/0_frp.py
@@ -28,6 +28,23 @@ def create_plotly_map(date: str) -> None:
     # heading
     st.subheader(f"Firepower on {date} across USA")
     st.plotly_chart(fig)
+def create_plotly_map_1(date: str) -> None:
+    file_name = date + "_geodata.gpkg"
+    # csv_filename = date + "_final_merged.csv"
+    # csv = pd.read_csv(csv_filename)
+    merged_geo_data = gpd.read_file(file_name)
+    fig = px.choropleth(merged_geo_data, geojson=merged_geo_data.geometry, locations= merged_geo_data.index, color=merged_geo_data.sum_frprpl,
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 2),
+                           scope="usa",
+                           labels={'frp':'FirePower'},
+                           hover_data={'COUNTY': True, 'FIPS': True}
+                          )
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}) 
+    # set the plot
+    # heading
+    st.subheader(f"Firepower on {date} across USA")
+    st.plotly_chart(fig)
 
 st.title("Map Visualizations")
 # set a dropdown
@@ -39,11 +56,16 @@ date = st.selectbox(
 
 if date == DATE_ONE:
     create_plotly_map(date)
+    create_plotly_map_1(date)
 elif date == DATE_TWO:
     create_plotly_map(date)
+    create_plotly_map_1(date)
 elif date == DATE_THREE:
     create_plotly_map(date)
+    create_plotly_map_1(date)
 elif date == DATE_FOUR:
     create_plotly_map(date)
+    create_plotly_map_1(date)
 elif date == DATE_FIVE:
     create_plotly_map(date)
+    create_plotly_map_1(date)
