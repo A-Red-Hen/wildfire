fbbe397b28e1dc43f8ef07e5d2cba970cc999c0a
A-Red-Hen <99688148+A-Red-Hen@users.noreply.github.com>
2024-04-06 02:51:10 +0000

    fixed error

--- pages/2_additional_resources.py
+++ pages/2_additional_resources.py
@@ -1,6 +1,15 @@
+import streamlit as st
+
 st.title('Additional Resources')
 
 st.header('Additional Information')
 # references
 
 st.header('Dataset Sources')
+st.markdown("1. (%s)" % "https://zenodo.org/records/5636429")
+st.markdown("2. (%s)" % "https://www.atsdr.cdc.gov/placeandhealth/svi/data_documentation_download.html")
+
+st.header('Project Videos')
+st.markdown("**Technovation Girls 2024 Project Videos**")
+st.markdown("Pitch Video: (%s)" % "https://www.youtube.com/")
+st.markdown("Demo Video: (%s)" % "https://www.youtube.com/")
