bfa3a36b4d10faa938f05577005941f06b13cf91
A-Red-Hen <99688148+A-Red-Hen@users.noreply.github.com>
2024-04-06 05:07:54 +0000

    revert

--- pages/2_Additional_Resources.py
+++ pages/2_Additional_Resources.py
@@ -1,1 +1,15 @@
 import streamlit as st
+
+st.title('Additional Resources')
+
+st.header('Project Videos')
+st.markdown("**Technovation Girls 2024 Project Videos**")
+st.markdown("Pitch Video: %s" % "https://www.youtube.com/")
+st.markdown("Demo Video: %s" % "https://www.youtube.com/")
+
+st.header('Dataset Sources')
+st.markdown("1. %s" % "https://zenodo.org/records/5636429")
+st.markdown("2. %s" % "https://www.atsdr.cdc.gov/placeandhealth/svi/data_documentation_download.html")
+
+st.header('References')
+# references
