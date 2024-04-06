ba539ee43079741097c8e3d47a66f6787a5142d5
A-Red-Hen <99688148+A-Red-Hen@users.noreply.github.com>
2024-04-06 03:08:08 +0000

    test

--- pages/1_information.py
+++ pages/1_information.py
@@ -9,7 +9,7 @@ st.markdown('The AI model predicts the wildfire intensity based on meteorologica
 st.header('Social Vulnerability')
 st.markdown('**Social Vulnerability** shows the extent to which a community has social conditions, e.g. high poverty, high percentage of disabled population or low transportation access. These conditions may affect the community’s ability to prevent human suffering (mortality, not being able to recover from financial loss, losing their homes, getting displaced from the community) during disaster events.')
 st.markdown('The social vulnerability data for &lt;project name&gt; was obtained from the Centers for Disease Control and Prevention’s CDC SVI Database, which ranks all the ~80,000 census tracts across the USA based on their social vulnerability index (SVI). SVI is calculated for each census tract based on the 15 factors shown in below picture. The SVI value for each tract will be a real number in the range of 0.0 to 1.0, with the least socially vulnerable tracts having a value of 0.0 and the most socially vulnerable tracts having a value of 1.0.')
-img1 = "/workspaces/wildfire/img1.png"
+img1 = "img1.png"
 st.image(img1, caption="<caption>")
 
 st.header('Combined Vulnerability')
